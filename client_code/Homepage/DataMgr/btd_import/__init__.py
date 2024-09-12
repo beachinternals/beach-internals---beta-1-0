ff50c36ab4cbcb6b06ba9a6b7e1b9cb86eae1125
spccoach@gmail.com <spccoach@gmail.com>
2024-09-12 23:17:16 +0000

    Created ServerModule1; Edited Homepage, Homepage.AboutUs, Homepage.BetaDevLog, Homepage.Contact and 27 more

--- client_code/Homepage/DataMgr/btd_import/__init__.py
+++ client_code/Homepage/DataMgr/btd_import/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import btd_importTemplate
 from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
@@ -14,3 +15,149 @@ class btd_import(btd_importTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    # find out team for this user
+    user_row = anvil.users.get_user()
+    if not user_row:
+      alert("Not Currently Logged in")
+      open_form('Homepage.Landing_form')
+
+    
+    user_team = user_row["team"]
+  
+    # Now, let's populate the drop downs. 
+    self.league_drop_down.selected_value = user_row["def_league"]
+    self.gender_drop_down.selected_value = user_row["def_gender"]
+    self.year_drop_down.selected_value = user_row["def_year"]
+
+    print(f"League, gender, year, selected values:{self.league_drop_down.selected_value}, {self.gender_drop_down.selected_value},{self.year_drop_down.selected_value} ")
+  
+    # populate the drop downs for league, and competition level 1 and 3
+    self.league_drop_down.items = [(row["league"], row) for row in app_tables.league_list.search()]
+    #print(f"League Drop Down Items {self.league_drop_down.items}")
+    # for competition level 1
+    self.comp_l1_drop_down.items = [(row["comp_l1"], row) for row in app_tables.league_comp_l1.search( league = self.league_drop_down.selected_value['league'] )]
+    
+    # for Competition Level 3, need to serach the selected league's playoff structure (flight, playoffs)
+    self.comp_l3_drop_down.items = [(row["comp_l3"], row) for row in app_tables.league_comp_l3.search( comp_l3_label = self.league_drop_down.selected_value['comp_l3_label'])]
+    
+  pass
+
+  def league_drop_down_change(self, **event_args):
+    """This method is called when an item is selected"""
+    self.comp_l1_drop_down.items = [(row["comp_l1"], row) for row in app_tables.league_comp_l1.search( league = self.league_drop_down.selected_value['league'] )]
+
+    #print(f"league drop down value:{self.league_drop_down.selected_value['league']}")
+    if "NCAA" in self.league_drop_down.selected_value['league']:
+      self.gender_drop_down.selected_value = "W"
+      #print("changed gender to W")
+      
+    # for Competition Level 3, need to serach the selected league's playoff structure (flight, playoffs)
+    self.comp_l3_drop_down.items = [(row["comp_l3"], row) for row in app_tables.league_comp_l3.search( comp_l3_label = self.league_drop_down.selected_value['comp_l3_label'])]
+    
+    pass
+
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+     # calcuate the information on the uploaded btd file
+    return_value =  anvil.server.call('update_btd_characteristics', file )
+
+    print(f"players in the client, {return_value[0]} , {return_value[1]} , {return_value[2]} , {return_value[3]}")
+    print(f"League Dropdown selected value:{self.league_drop_down.selected_value}")
+    
+    player_list = [ return_value[0], return_value[1], return_value[2], return_value[3] ]
+    
+    self.btd_playera1_drop_down.items = player_list
+    self.btd_playera2_drop_down.items = player_list
+    #self.btd_playerb1_drop_down.items = player_list
+    self.btd_playerb2_drop_down.items = player_list
+    self.btd_playerb1_drop_down.items = player_list
+
+    self.btd_playera1_drop_down.selected_value = return_value[0]
+    self.btd_playera2_drop_down.selected_value = return_value[1]
+    self.btd_playerb1_drop_down.selected_value = return_value[2]
+    self.btd_playerb2_drop_down.selected_value = return_value[3]
+
+    # Set the drop down for the ppr players
+    ppr_list = [
+      {
+        "team":   row["team"],
+        "number":   row["number"],
+        "shortname": row["shortname"],
+      }
+      for row in app_tables.master_player.search(
+        league = self.league_drop_down.selected_value['league'],
+        year = self.year_drop_down.selected_value,
+        gender=self.gender_drop_down.selected_value
+      )
+    ]
+    ppr_player_list = []
+    for i in ppr_list:
+      ppr_player_list.append( i['team']+" "+i['number']+" "+i['shortname'] )
+    
+    self.ppr_playera1_drop_down.items = ppr_player_list
+    self.ppr_playera2_drop_down.items = ppr_player_list
+    self.ppr_playerb1_drop_down.items = ppr_player_list
+    self.ppr_playerb2_drop_down.items = ppr_player_list
+    
+    # save the other values
+    self.points_label.text = return_value[4]
+    self.comp_score_label.text = return_value[5]
+    self.per_players_label.text = return_value[6]
+    self.per_coord_label.text = return_value[7] 
+    self.per_srv_player_label.text = return_value[8]
+    self.filename_box.text = file.name
+    pass
+
+  def outlined_button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert("Saving your data now")
+
+    # pull out the correct selected value from the comp3 selected value row
+    #print(f" self comp_l3 drop down selected value: {self.comp_l3_drop_down.selected_value}")
+    #print(f"size of selected value row: {len(self.comp_l3_drop_down.selected_value)}")
+    #print(f" selected value 0 {self.comp_l3_drop_down.selected_value[0]}")
+    #print(f" selected value 1 {self.comp_l3_drop_down.selected_value[1]}")
+    #print(f" selected value 2 {self.comp_l3_drop_down.selected_value[2]}")
+
+    print(self.comp_l3_drop_down.selected_value)
+    print(f"com_l3 data:{self.comp_l3_drop_down.selected_value['comp_l3']}")
+
+    # creat a new row with the data
+    print(f"per players            :{self.per_players_label.text}")
+    print(f"per player -1          :{self.per_players_label.text[:-1]}")
+    print(f"float of per player -1 :{float(self.per_players_label.text[:-1])}")
+    per_play = float(self.per_players_label.text[:-1])
+    print(f"percent players, per_play:{per_play}")
+    pts = int(self.points_label.text)
+    per_s_play = float(self.per_srv_player_label.text[:-1])
+    per_a_play = float(self.per_players_label.text[:-1])
+    
+    app_tables.btd_files.add_row( 
+      league=self.league_drop_down.selected_value["league"],
+      gender=self.gender_drop_down.selected_value,
+      year=self.year_drop_down.selected_value,
+      comp_l1=self.comp_l1_drop_down.selected_value["comp_l1"],
+      comp_l2=self.comp_l2_box.text,
+      comp_l3=self.comp_l3_drop_down.selected_value["comp_l3"],
+      date=self.date_picker.date,
+      filename=self.file_loader_1.file.name,
+      csv_data=self.file_loader_1.file,
+      team=anvil.users.get_user()['team'],
+      points = pts,
+      per_players = per_play,
+      per_srv_players = per_s_play,
+      per_xy = float(self.per_coord_label.text),
+      completeness_score = float(self.comp_score_label.text),
+      include_in_master = True,
+      player1 = self.btd_playera1_drop_down.selected_value,
+      player2 = self.btd_playera2_drop_down.selected_value,
+      player3 = self.btd_playerb1_drop_down.selected_value,
+      player4 = self.btd_playerb2_drop_down.selected_value,
+      ppr_playera1 = self.ppr_playera1_drop_down.selected_value,
+      ppr_playera2 = self.ppr_playera2_drop_down.selected_value,
+      ppr_playerb1 = self.ppr_playerb1_drop_down.selected_value,
+      ppr_playerb2 = self.ppr_playerb2_drop_down.selected_value
+    )
+    alert("New Row Saved!")
+    open_form("Homepage.DataMgr")
+
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,93 @@
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
+import anvil.server
+import pandas as pd
+import io
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+
+# ############ server function to calculate completeness for all btd file entries
+@anvil.server.callable
+def update_btd_characteristics( file ):
+
+  # set the user qualities
+  if anvil.users.get_user() is None :
+    user_team = ""
+    user_gender = ""
+    user_email = ""
+    user_league = ""
+    user_year = ""
+  else:
+    user_team = anvil.users.get_user()['team']
+    user_league = anvil.users.get_user()['def_league']
+    user_gender = anvil.users.get_user()['def_gender']
+    user_year = anvil.users.get_user()['def_year']
+    user_email = anvil.users.get_user()['email']
+    
+  # pass in the btd csv file, then return the characterestics
+  print("file:", file)
+  
+  # take file, and make the 
+  file_obj = io.BytesIO( file.get_bytes() )
+  btd_df = pd.read_csv(file_obj)
+
+  # Calculate number of actions
+  num_actions = int(btd_df.shape[0])
+
+  # Caculate the number of serves (points)
+  serves_df = btd_df[btd_df['action_type'] == "serve" ]
+  num_serves = int(serves_df.shape[0])
+
+  # Calculate number of actions with a player
+  players_df = btd_df[btd_df['player'].notna() ]
+  per_action_players = int(players_df.shape[0]) / num_actions
+
+  # calcaulte the number of serves with players idendified
+  srv_players = serves_df[serves_df['player'].notna()]
+  per_srv_players = int(srv_players.shape[0]) / num_serves
+  
+  # calculate number of actions with coordinates   
+  src_coord = btd_df[btd_df['src_zone'].notna()]
+  dest_coord = btd_df[btd_df['dest_zone'].notna()]
+  num_src_coord = int(src_coord.shape[0]) + int(dest_coord.shape[0])
+  per_coord = num_src_coord / (2*num_actions)
+  
+  # create a 'score' for completeness
+  comp_score = (5*per_srv_players + 3*per_action_players + 2*per_coord)/10
+  comp_score = str('{:.2%}'.format(comp_score))
+  per_coord = str('{:.2%}'.format(per_coord))
+  per_srv_players = str('{:.2%}'.format(per_srv_players))
+  per_action_players = str('{:.2%}'.format(per_action_players))
+  
+  #print(f"how are we doing?  Actions: {num_actions} Serves: {num_serves}, % Srv Players: {per_srv_players} % Coordiantes {per_coord} % Acion Players {per_action_players} Completion Score: {comp_score}")
+  #print("  ")
+
+    # now find the four or more) players in the file
+  players_unique = players_df.player.unique()
+  players_unique.sort()
+  num_players = players_unique.shape[0]
+
+  playera1 = players_unique[0]
+  playera2 = players_unique[1]
+  playerb1 = players_unique[2]
+  playerb2 = players_unique[3]
+  
+  # print(f"number of players: {num_players} List of players")
+  #print(players_unique, playera1, playera2, playerb1, playerb2 )
+
+  # now let's try to set the self.item data bindings to display this new data
+  
+  return [playera1, playera2, playerb1, playerb2, num_serves, comp_score, per_action_players, per_coord, per_srv_players]
+
+
+
