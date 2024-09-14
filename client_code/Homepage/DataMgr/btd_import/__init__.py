057457c765817e925e8d7552ec8d5bd72f80bb29
spccoach@gmail.com <spccoach@gmail.com>
2024-09-14 21:07:56 +0000

    Edited Homepage.DataMgr, settings and Homepage.DataMgr.btd_import

--- client_code/Homepage/DataMgr/btd_import/__init__.py
+++ client_code/Homepage/DataMgr/btd_import/__init__.py
@@ -28,11 +28,15 @@ class btd_import(btd_importTemplate):
     self.league_drop_down.selected_value = user_row["def_league"]
     self.gender_drop_down.selected_value = user_row["def_gender"]
     self.year_drop_down.selected_value = user_row["def_year"]
+    self.league2_drop_down.selected_value = user_row["def_league"]
+    self.gender2_drop_down.selected_value = user_row["def_gender"]
+    self.year2_drop_down.selected_value = user_row["def_year"]
 
-    print(f"League, gender, year, selected values:{self.league_drop_down.selected_value}, {self.gender_drop_down.selected_value},{self.year_drop_down.selected_value} ")
+    #print(f"League, gender, year, selected values:{self.league_drop_down.selected_value}, {self.gender_drop_down.selected_value},{self.year_drop_down.selected_value} ")
   
     # populate the drop downs for league, and competition level 1 and 3
     self.league_drop_down.items = [(row["league"], row) for row in app_tables.league_list.search()]
+    self.league2_drop_down.items = self.league_drop_down.items
     #print(f"League Drop Down Items {self.league_drop_down.items}")
     # for competition level 1
     self.comp_l1_drop_down.items = [(row["comp_l1"], row) for row in app_tables.league_comp_l1.search( league = self.league_drop_down.selected_value['league'] )]
@@ -53,7 +57,7 @@ class btd_import(btd_importTemplate):
       
     # for Competition Level 3, need to serach the selected league's playoff structure (flight, playoffs)
     self.comp_l3_drop_down.items = [(row["comp_l3"], row) for row in app_tables.league_comp_l3.search( comp_l3_label = self.league_drop_down.selected_value['comp_l3_label'])]
-    
+    self.league2_drop_down.selected_value = self.league_drop_down.selected_value
     pass
 
   def file_loader_1_change(self, file, **event_args):
@@ -108,7 +112,7 @@ class btd_import(btd_importTemplate):
     self.filename_box.text = file.name
     pass
 
-  def outlined_button_2_click(self, **event_args):
+  def save_file_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     alert("Saving your data now")
 
@@ -164,5 +168,72 @@ class btd_import(btd_importTemplate):
   def private_help_link_click(self, **event_args):
     """This method is called when the link is clicked"""
     alert("This defines how the data will be user: /n Private - Only avaiable to the team, used for Player and Pair reports. /n Public - used for scouting, avaialble to all teams on the Internals network.")
+    pass
+
+  def save_player_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # this is to save a new player in the master player database.
+    # First, check if the player exists
+    p_rows = app_tables.master_player.search(
+      league = self.league2_drop_down.selected_value['league'],
+      gender = self.gender2_drop_down.selected_value,
+      year=self.year2_drop_down.selected_value,
+      team = self.team2_text_box.text,
+      number=self.number_text_box.text,
+      shortname=self.short_name_text_box.text
+      )
+
+    if len(p_rows) > 0:
+      alert(title='Player Elready Exists')
+      return False
+
+    add_row = app_tables.master_player.add_row(
+      league = self.league2_drop_down.selected_value['league'],
+      gender = self.gender2_drop_down.selected_value,
+      year=self.year2_drop_down.selected_value,
+      team = self.team2_text_box.text,
+      number=self.number_text_box.text,
+      shortname=self.short_name_text_box.text,
+      fullname=self.full_name_text_box.text
+    )
+
+    if add_row:
+      alert(title='Player Added Successfully')
+
+    # now update the drop down menus for players
+    
+    # Set the drop down for the ppr players
+    ppr_list = [
+      {
+        "team":   row["team"],
+        "number":   row["number"],
+        "shortname": row["shortname"],
+      }
+      for row in app_tables.master_player.search(
+        league = self.league_drop_down.selected_value['league'],
+        year = self.year_drop_down.selected_value,
+        gender=self.gender_drop_down.selected_value
+      )
+    ]
+    ppr_player_list = []
+    for i in ppr_list:
+      ppr_player_list.append( i['team']+" "+i['number']+" "+i['shortname'] )
+    
+    self.ppr_playera1_drop_down.items = ppr_player_list
+    self.ppr_playera2_drop_down.items = ppr_player_list
+    self.ppr_playerb1_drop_down.items = ppr_player_list
+    self.ppr_playerb2_drop_down.items = ppr_player_list
+    pass
+
+  def year_drop_down_change(self, **event_args):
+    """This method is called when an item is selected"""
+    # set the add player year to the same year
+    self.year2_drop_down.selected_value = self.year_drop_down.selected_value
+    pass
+
+  def gender_drop_down_change(self, **event_args):
+    """This method is called when an item is selected"""
+    # set thegender to the same for the add player link
+    self.gender2_drop_down.selected_value = self.gender_drop_down.selected_value
     pass
 
