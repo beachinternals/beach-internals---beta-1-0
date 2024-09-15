50d7d9fd41902325bca229bfb1543b23cf0d1a4b
spccoach@gmail.com <spccoach@gmail.com>
2024-09-15 17:15:10 +0000

    Edited Homepage.DataMgr.btd_manage

--- client_code/Homepage/DataMgr/btd_manage/__init__.py
+++ client_code/Homepage/DataMgr/btd_manage/__init__.py
@@ -30,6 +30,16 @@ class btd_manage(btd_manageTemplate):
     # for competition level 1
     self.comp_l1_drop_down.items = [(row["comp_l1"], row) for row in app_tables.league_comp_l1.search( league = self.league_drop_down.selected_value['league'] )]
 
+    self.repeating_panel_1.items = app_tables.btd_files.search(
+      league=self.league_drop_down.selected_value['league'],
+      gender=self.gender_drop_down.selected_value,
+      year=self.year_drop_down.selected_value,
+      comp_l1=self.comp_l1_drop_down.selected_value['comp_l1'],
+      team=user_row['team']
+    )
+    self.team_label.text = anvil.users.get_user()['team']
+  pass
+  
   def league_drop_down_change(self, **event_args):
     """This method is called when an item is selected"""
     self.comp_l1_drop_down.items = [(row["comp_l1"], row) for row in app_tables.league_comp_l1.search( league = self.league_drop_down.selected_value['league'] )]
@@ -37,5 +47,46 @@ class btd_manage(btd_manageTemplate):
     #print(f"league drop down value:{self.league_drop_down.selected_value['league']}")
     if "NCAA" in self.league_drop_down.selected_value['league']:
       self.gender_drop_down.selected_value = "W"
- 
+
+    self.repeating_panel_1.items = app_tables.btd_files.search(
+      league=self.league_drop_down.selected_value['league'],
+      gender=self.gender_drop_down.selected_value,
+      year=self.year_drop_down.selected_value,
+      comp_l1=self.comp_l1_drop_down.selected_value['comp_l1'],
+      team=anvil.users.get_user()['team']
+    )
+      
+    pass
+
+  def gender_drop_down_change(self, **event_args):
+    """This method is called when an item is selected"""
+    self.repeating_panel_1.items = app_tables.btd_files.search(
+      league=self.league_drop_down.selected_value['league'],
+      gender=self.gender_drop_down.selected_value,
+      year=self.year_drop_down.selected_value,
+      comp_l1=self.comp_l1_drop_down.selected_value['comp_l1'],
+      team=anvil.users.get_user()['team']
+    )
+    pass
+
+  def year_drop_down_change(self, **event_args):
+    """This method is called when an item is selected"""
+    self.repeating_panel_1.items = app_tables.btd_files.search(
+      league=self.league_drop_down.selected_value['league'],
+      gender=self.gender_drop_down.selected_value,
+      year=self.year_drop_down.selected_value,
+      comp_l1=self.comp_l1_drop_down.selected_value['comp_l1'],
+      team=anvil.users.get_user()['team']
+    )
+    pass
+
+  def comp_l1_drop_down_change(self, **event_args):
+    """This method is called when an item is selected"""
+    self.repeating_panel_1.items = app_tables.btd_files.search(
+      league=self.league_drop_down.selected_value['league'],
+      gender=self.gender_drop_down.selected_value,
+      year=self.year_drop_down.selected_value,
+      comp_l1=self.comp_l1_drop_down.selected_value['comp_l1'],
+      team=anvil.users.get_user()['team']
+    )
     pass
