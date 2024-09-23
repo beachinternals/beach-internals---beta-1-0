6bd8cf1a8cdab6fa31fe572675685df1e6edf43c
spccoach@gmail.com <spccoach@gmail.com>
2024-09-23 13:27:24 +0000

    Edited Homepage.PlayerRpt

--- client_code/Homepage/PlayerRpt/__init__.py
+++ client_code/Homepage/PlayerRpt/__init__.py
@@ -26,6 +26,26 @@ class PlayerRpt(PlayerRptTemplate):
     elif not user_row["team"]:
       alert('Please Contact Beach Internals to be Assigned to a Team')
       open_form('Homepage.Contact')
+
+    # First, populate the selected values
+    self.league_drop_down.selected_value = user_row["def_league"]+'|'+user_row['def_gender']+'|'+user_row['def_year']
+    self.league_drop_down.items = list(set([(r['league'])+' | '+r['gender']+' | '+r['year'] for r in app_tables.subscriptions.search(team=user_row['team'])]))
+
+    # populate the drop downs for league, and competition level 1 and 3
+    self.comp_l1_drop_down.items = [(row["comp_l1"], row) for row in app_tables.league_comp_l1.search( league = user_row["def_league"] )]
+    self.comp_l2_drop_down.items = [(row['comp_l2'], row) for row in app_tables.league_comp_l2.search( league= user_row['def_league'] )]
+    self.comp_l3_drop_down.items = [(row["comp_l3"], row) for row in app_tables.league_comp_l3.search( comp_l3_label = user_row['def_league'])]
+
+    # populate the player drop down
+    self.player_drop_down.items = [
+      (row["team"] + " " + row["number"] + " " + row["shortname"], row)
+      for row in app_tables.master_player.search(
+        tables.order_by("team"),
+        league=user_row['def_league'],
+        gender=user_row['def_gender'],
+        year=user_row['def_year'],
+      )
+    ]
   
   def PlayerRpt1_click_click(self, **event_args):
     """This method is called when the link is clicked"""
@@ -45,6 +65,102 @@ class PlayerRpt(PlayerRptTemplate):
     self.outlined_card_3.add_component(roster_manage())
     pass
 
-  def comp_1_drop_down_change(self, **event_args):
+  def comp_l1_drop_down_change(self, **event_args):
     """This method is called when an item is selected"""
+    # if we change the competition level 1, this will change the options for competition level 2
+    
+    # extract league, gender, year from league selected value
+    league_value = self.league_drop_down.selected_value
+    str_loc = league_value.index('|')
+    disp_league = league_value[:str_loc-1].strip()
+    league_value = league_value[str_loc+1:]
+    str_loc = league_value.index('|')
+    disp_gender = league_value[:str_loc-1].strip()
+    disp_year = league_value[str_loc+1:].strip()
+    
+    # set comp_l2 data:    
+    self.comp_l2_drop_down.items = [(row['comp_l2'], row) for row in app_tables.league_comp_l2.search( 
+      league= disp_league,
+      comp_l1=self.comp_l1_drop_down.selected_value['comp_l1']
+    )]
+    
+    # set comp_l3 data:
+    comp3lbl = [(r['comp_l3_label'],r) for r in app_tables.league_list.search(league=disp_league)]
+    self.comp_l3_drop_down.items = [(row["comp_l3"], row) for row in app_tables.league_comp_l3.search( comp_l3_label = comp3lbl[0][0])]
+
+    pass
+
+  def league_drop_down_change(self, **event_args):
+    """This method is called when an item is selected"""
+    # extract league, gender, year from league selected value
+    league_value = self.league_drop_down.selected_value
+    str_loc = league_value.index('|')
+    disp_league = league_value[:str_loc-1].strip()
+    league_value = league_value[str_loc+1:]
+    str_loc = league_value.index('|')
+    disp_gender = league_value[:str_loc-1].strip()
+    disp_year = league_value[str_loc+1:].strip()
+
+    # set comp_l1 data:
+    self.comp_l1_drop_down.items = [(row["comp_l1"], row) for row in app_tables.league_comp_l1.search( league = disp_league )]
+
+    # set comp_l2 data:
+    self.comp_l2_drop_down.items = [(row['comp_l2'], row) for row in app_tables.league_comp_l2.search( 
+      league= disp_league,
+      comp_l1=self.comp_l1_drop_down.selected_value['comp_l1']
+    )]
+    
+    # set comp_l3 data:
+    comp3lbl = [(r['comp_l3_label'],r) for r in app_tables.league_list.search(league=disp_league)]
+    self.comp_l3_drop_down.items = [(row["comp_l3"], row) for row in app_tables.league_comp_l3.search( comp_l3_label = comp3lbl[0][0])]
+
+    self.player_drop_down.items = [
+      (row["team"] + " " + row["number"] + " " + row["shortname"], row)
+      for row in app_tables.master_player.search(
+        tables.order_by("team"),
+        league=disp_league,
+        gender=disp_gender,
+        year=disp_year,
+      )
+    ]
+    pass
+
+  def end_date_picker_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    pass
+
+  def generate_report_buttoon_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # unpack the league data:
+        # extract league, gender, year from league selected value
+    league_value = self.league_drop_down.selected_value
+    str_loc = league_value.index('|')
+    disp_league = league_value[:str_loc-1].strip()
+    league_value = league_value[str_loc+1:]
+    str_loc = league_value.index('|')
+    disp_gender = league_value[:str_loc-1].strip()
+    disp_year = league_value[str_loc+1:].strip()
+
+    # unpack the player
+    disp_player = self.player_drop_down.selected_value
+
+    # unpack the source data:
+    user_row = anvil.users.get_user(allow_remembered=True)
+    disp_team = user_row['team']
+
+    # unpack the report to process
+    # replace this with a data driven approach
+    report_name = self.report_drop_down.selected_value
+    if report_name == 'FBHE Along the Net':
+      if self.team_drop_down.selected_value == "Scouting":
+        function_name = 'fbhe_scout_query'
+      else:
+        function_name = 'fbhe_table_query'
+
+    # now, call the server modeul
+    table_data = anvil.server.call(function_name, disp_league, disp_gender, disp_year, disp_team, disp_player)
+
+    # now put this into the rtf box
+    self.rpt_disp_box.content = table_data
+    
     pass
