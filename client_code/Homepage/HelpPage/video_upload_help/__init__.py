f521dca8272f14c37419d2c6b6dee72d16d826a2
spccoach@gmail.com <spccoach@gmail.com>
2024-09-24 17:08:17 +0000

    Created btd upload 1.png, Homepage.HelpPage.video_upload_help, balltime upload.png and btd upload 0.png; Edited Homepage.HelpPage

--- client_code/Homepage/HelpPage/video_upload_help/__init__.py
+++ client_code/Homepage/HelpPage/video_upload_help/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import HelpPageTemplate
+from ._anvil_designer import video_upload_helpTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -7,17 +7,11 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import video_upload_help
 
-class HelpPage(HelpPageTemplate):
+
+class video_upload_help(video_upload_helpTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def video_upload_help_link_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    self.outlined_card_2.clear()
-    self.outlined_card_2.add_component(video_upload_help())
-    pass
