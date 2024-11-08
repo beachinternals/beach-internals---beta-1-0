dcd55d0329e66128aaf88202e80baa83d9205877
spccoach@gmail.com <spccoach@gmail.com>
2024-11-08 13:15:16 +0000

    Edited dashboard and Homepage.Dashboard

--- server_code/dashboard.py
+++ server_code/dashboard.py
@@ -64,58 +64,70 @@ def coaches_dashboard(league_value, disp_team):
   disp_df = player_data_df[['team','player','fbhe','fbhe1', 'fbhe2', 'fbhe3', 'fbhe4', 'fbhe5']]
   mean_df1_dict = {'team':['Mean:'],
                          'player':[' '],
-                         'fbhe':[disp_df['fbhe'].mean(skipna=True)],
-                         'fbhe1':[disp_df['fbhe1'].mean(skipna=True)],
-                         'fbhe2':[disp_df['fbhe2'].mean(skipna=True)],
-                         'fbhe3':[disp_df['fbhe3'].mean(skipna=True)],
-                         'fbhe4':[disp_df['fbhe4'].mean(skipna=True)],
-                         'fbhe5':[disp_df['fbhe5'].mean(skipna=True)] }
+                         'fbhe':["{:.3f}".format(disp_df['fbhe'].mean(skipna=True))],
+                         'fbhe1':["{:.3f}".format(disp_df['fbhe1'].mean(skipna=True))],
+                         'fbhe2':["{:.3f}".format(disp_df['fbhe2'].mean(skipna=True))],
+                         'fbhe3':["{:.3f}".format(disp_df['fbhe3'].mean(skipna=True))],
+                         'fbhe4':["{:.3f}".format(disp_df['fbhe4'].mean(skipna=True))],
+                         'fbhe5':["{:.3f}".format(disp_df['fbhe5'].mean(skipna=True))] }
   mean_df1 = pd.DataFrame.from_dict(mean_df1_dict)
   sd_df1_dict = {'team':['St Dev:'],
                          'player':[' '],
-                         'fbhe':[disp_df['fbhe'].std(skipna=True)],
-                         'fbhe1':[disp_df['fbhe1'].std(skipna=True)],
-                         'fbhe2':[disp_df['fbhe2'].std(skipna=True)],
-                         'fbhe3':[disp_df['fbhe3'].std(skipna=True)],
-                         'fbhe4':[disp_df['fbhe4'].std(skipna=True)],
-                         'fbhe5':[disp_df['fbhe5'].std(skipna=True)] }
+                         'fbhe':["{:.3f}".format(disp_df['fbhe'].std(skipna=True))],
+                         'fbhe1':["{:.3f}".format(disp_df['fbhe1'].std(skipna=True))],
+                         'fbhe2':["{:.3f}".format(disp_df['fbhe2'].std(skipna=True))],
+                         'fbhe3':["{:.3f}".format(disp_df['fbhe3'].std(skipna=True))],
+                         'fbhe4':["{:.3f}".format(disp_df['fbhe4'].std(skipna=True))],
+                         'fbhe5':["{:.3f}".format(disp_df['fbhe5'].std(skipna=True))] }
   sd_df1 = pd.DataFrame.from_dict(sd_df1_dict)
   disp_df = pd.concat([disp_df,mean_df1,sd_df1])
   df_table1 = pd.DataFrame.to_markdown(disp_df, index=False )
 
   disp_df = player_data_df[['player','srv1_fbhe','srv3_fbhe','srv5_fbhe']]
   mean_df2_dict = {'player':['Mean:'],
-                         'srv1_fbhe':[disp_df['srv1_fbhe'].mean(skipna=True)],
-                         'srv3_fbhe':[disp_df['srv3_fbhe'].mean(skipna=True)],
-                         'srv5_fbhe':[disp_df['srv5_fbhe'].mean(skipna=True)] }
+                         'srv1_fbhe':["{:.3f}".format(disp_df['srv1_fbhe'].mean(skipna=True))],
+                         'srv3_fbhe':["{:.3f}".format(disp_df['srv3_fbhe'].mean(skipna=True))],
+                         'srv5_fbhe':["{:.3f}".format(disp_df['srv5_fbhe'].mean(skipna=True))] }
   mean_df2 = pd.DataFrame.from_dict(mean_df2_dict)
   sd_df2_dict = {'player':['St Dev:'],
-                         'srv1_fbhe':[disp_df['srv1_fbhe'].std(skipna=True)],
-                         'srv3_fbhe':[disp_df['srv3_fbhe'].std(skipna=True)],
-                         'srv5_fbhe':[disp_df['srv5_fbhe'].std(skipna=True)] }
+                         'srv1_fbhe':["{:.3f}".format(disp_df['srv1_fbhe'].std(skipna=True))],
+                         'srv3_fbhe':["{:.3f}".format(disp_df['srv3_fbhe'].std(skipna=True))],
+                         'srv5_fbhe':["{:.3f}".format(disp_df['srv5_fbhe'].std(skipna=True))] }
   sd_df2 = pd.DataFrame.from_dict(sd_df2_dict)
   disp_df = pd.concat([disp_df,mean_df2,sd_df2])
   df_table2 = pd.DataFrame.to_markdown(disp_df, index=False )
   
   disp_df = player_data_df[['player','err_den','tcr','tcr_r','tcr_s','expected']]
   mean_df3_dict = {'player':['Mean:'],
-                         'err_den':[disp_df['err_den'].mean(skipna=True)],
-                         'tcr':[disp_df['tcr'].mean(skipna=True)],
-                         'tcr_r':[disp_df['tcr_r'].mean(skipna=True)],
-                         'tcr_s':[disp_df['tcr_s'].mean(skipna=True)],
-                         'expected':[disp_df['expected'].mean(skipna=True)] }
+                         'err_den':["{:.1f}".format(disp_df['err_den'].mean(skipna=True))],
+                         'tcr':["{:.1f}".format(disp_df['tcr'].mean(skipna=True))],
+                         'tcr_r':["{:.1f}".format(disp_df['tcr_r'].mean(skipna=True))],
+                         'tcr_s':["{:.1f}".format(disp_df['tcr_s'].mean(skipna=True))],
+                         'expected':["{:.1f}".format(disp_df['expected'].mean(skipna=True))] }
   mean_df3 = pd.DataFrame.from_dict(mean_df3_dict)
   sd_df3_dict = {'player':['St Dev:'],
-                         'err_den':[disp_df['err_den'].std(skipna=True)],
-                         'tcr':[disp_df['tcr'].std(skipna=True)],
-                         'tcr_r':[disp_df['tcr_r'].std(skipna=True)],
-                         'tcr_s':[disp_df['tcr_s'].std(skipna=True)],
-                         'expected':[disp_df['expected'].std(skipna=True)] }
+                         'err_den':["{:.2f}".format(disp_df['err_den'].std(skipna=True))],
+                         'tcr':["{:.2f}".format(disp_df['tcr'].std(skipna=True))],
+                         'tcr_r':["{:.2f}".format(disp_df['tcr_r'].std(skipna=True))],
+                         'tcr_s':["{:.2f}".format(disp_df['tcr_s'].std(skipna=True))],
+                         'expected':["{:.2f}".format(disp_df['expected'].std(skipna=True))] }
   sd_df3 = pd.DataFrame.from_dict(sd_df3_dict)
   disp_df = pd.concat([disp_df,mean_df3,sd_df3])
   df_table3 = pd.DataFrame.to_markdown(disp_df, index=False )
 
-  player_stats_disp = player_stats_df[['fbhe_mean','fbhe_range_mean','err_den_mean','tcr_mean','tcr_r_mean','tcr_s_mean','expected_mean']]
+  player_stats_disp = {
+    'Value':['Mean','St Dev'],
+    'FBHE':["{:.3f}".format(player_stats_df.at[0,'fbhe_mean']),"{:.3f}".format(player_stats_df.at[0,'fbhe_stdev'])] ,
+#    'FBHE - From 1':["{:.3f}".format(player_stats_df.at[0,'srv1_fbhe_mean']),"{:.3f}".format(player_stats_df.at[0,'srv1_fbhe_stdev'])] ,
+#    'FBHE - From 3':["{:.3f}".format(player_stats_df.at[0,'srv3_fbhe_mean']),"{:.3f}".format(player_stats_df.at[0,'srv3_fbhe_stdev'])] ,
+#    'FBHE - From 5':["{:.3f}".format(player_stats_df.at[0,'srv5_fbhe_mean']),"{:.3f}".format(player_stats_df.at[0,'srv5_fbhe_stdev'])] ,
+    'Expected Value':["{:.1f}".format(player_stats_df.at[0,'expected_mean']),"{:.1f}".format(player_stats_df.at[0,'expected_stdev'])],
+    'Err Den':["{:.1f}".format(player_stats_df.at[0,'err_den_mean']),"{:.1f}".format(player_stats_df.at[0,'err_den_stdev'])] ,
+    'Transition Conversion':["{:.1f}".format(player_stats_df.at[0,'tcr_mean']),"{:.1f}".format(player_stats_df.at[0,'tcr_stdev'])] ,
+    'TCR - Serving':["{:.1f}".format(player_stats_df.at[0,'tcr_s_mean']),"{:.1f}".format(player_stats_df.at[0,'tcr_s_stdev'])] ,
+    'TCR - Receiving':["{:.1f}".format(player_stats_df.at[0,'tcr_r_mean']),"{:.1f}".format(player_stats_df.at[0,'tcr_r_stdev'])] 
+
+  }
   df_stats_table = pd.DataFrame.to_markdown(player_stats_disp, index=False)
   
   return df_table1, df_table2, df_table3, df_stats_table
