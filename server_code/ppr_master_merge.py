1e670c299875c26b30bc40e7b10483945399015d
spccoach@gmail.com <spccoach@gmail.com>
2024-11-19 18:55:44 +0000

    Edited Homepage, Homepage.AboutUs, Homepage.BetaDevLog, Homepage.Contact and 41 more

--- server_code/ppr_master_merge.py
+++ server_code/ppr_master_merge.py
@@ -4,7 +4,6 @@ from anvil.google.drive import app_files
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
 import anvil.server
 import pandas as pd
 import io
