ef15883b4e251e2b9d4f05a09531e4ade0a27c29
spccoach@gmail.com <spccoach@gmail.com>
2024-12-19 16:50:43 +0000

    Edited fbhe_query

--- server_code/fbhe_query.py
+++ server_code/fbhe_query.py
@@ -889,6 +889,75 @@ def expected_value(disp_league, disp_gender, disp_year,
   return ev_return, ' ', ' '
 
 @anvil.server.callable
+def out_of_system(disp_league, disp_gender, disp_year, 
+                    disp_team, disp_player,
+                    comp_l1_checked, disp_comp_l1,
+                    comp_l2_checked, disp_comp_l2,
+                    comp_l3_checked, disp_comp_l3,
+                    date_checked, disp_start_date, disp_end_date,
+                    scout, explain_text
+               ):
+  # return a markdown text to display
+  # given the parameters
+
+  ############## First - Get the Data, and limit it by the parameters - Generaic for all reports
+  m_ppr_df = get_ppr_data( disp_league, disp_gender, disp_year, disp_team, scout )
+  m_ppr_df = ppr_df_limit( m_ppr_df, 
+                          comp_l1_checked, disp_comp_l1, 
+                          comp_l2_checked, disp_comp_l2, 
+                          comp_l3_checked, disp_comp_l3, 
+                          date_checked, disp_start_date, disp_end_date
+                         )
+    
+  #print(f"master scout data frame (after filter):{m_ppr_df.shape}, display player:{disp_player} m ppr df 0:{m_ppr_df.shape[0]}")
+
+  ############## Secomd - Create the dataframe that will be displayed as a table, report specific
+  # create the output dataframe - This is speficif to the report
+  df_dict = {' ':['FBHE','Kills','Errors','Attempts', ' ','URL'],
+             'All':[0,0,0,0,0,' '],
+             'Zone 1':[0,0,0,0,0,' '],
+             "Zone 2":[0,0,0,0,0,' '],
+             'Zone 3':[0,0,0,0,0,' '],
+             'Zone 4':[0,0,0,0,0,' '],
+             'Zone 5':[0,0,0,0,0,' ']
+            }
+  fbhe_table = pd.DataFrame.from_dict( df_dict )
+
+  # now limit to only out of system plays
+  oos_type = ['A','L','H']
+  m_ppr_df = m_ppr_df[ m_ppr_df['pass_oos'].str.strip() == 'L' or m_ppr_df['pass_oos'].str.strip() == 'A' ]
+  
+  ############### Third Populate the dataframe, assuming we have data returned
+  if m_ppr_df.shape[0] > 0:
+    # calculate fbhe for all attacks
+    #print(f"Calling fbhe:{m_ppr_df.shape}, {disp_player}")
+    fbhe_vector = fbhe( m_ppr_df, disp_player, 'att', True )
+    fbhe_table.at[0,'All'] = fbhe_vector[0]  # fbhe
+    fbhe_table.at[1,'All'] = fbhe_vector[1]  # attacks
+    fbhe_table.at[2,'All'] = fbhe_vector[2]  # errors
+    fbhe_table.at[3,'All'] = fbhe_vector[3]  # attempts
+    fbhe_table.at[4,'All'] = fbhe_vector[4]  # confidence interval
+    fbhe_table.at[5,'All'] = fbhe_vector[5]  # URL
+
+    # calculate for zones 1 - 5
+    column = ['Zone 1','Zone 2','Zone 3','Zone 4','Zone 5']
+    for i in [1,2,3,4,5]:
+      fbhe_vector = fbhe( m_ppr_df[m_ppr_df['att_src_zone_net']==i], disp_player, 'att', True )
+      fbhe_table.at[0,column[i-1]] = fbhe_vector[0]  # fbhe
+      fbhe_table.at[1,column[i-1]] = fbhe_vector[1]  # attacks
+      fbhe_table.at[2,column[i-1]] = fbhe_vector[2]  # errors
+      fbhe_table.at[3,column[i-1]] = fbhe_vector[3]  # attempts
+      fbhe_table.at[4,column[i-1]] = fbhe_vector[4]  # confidence interval
+      fbhe_table.at[5,column[i-1]] = fbhe_vector[5]  # URL
+ 
+    # now create the markdown text to return
+    fbhe_return = pd.DataFrame.to_markdown(fbhe_table)
+  else:
+    fbhe_return = "No Data Found"
+  
+  return fbhe_return, ' ', ' '
+
+@anvil.server.callable
 def report_stuba(disp_league, disp_gender, disp_year, 
                     disp_team, disp_player,
                     comp_l1_checked, disp_comp_l1,
