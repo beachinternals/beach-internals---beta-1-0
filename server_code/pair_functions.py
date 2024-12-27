918f797efcef7c84e82ce1565a71d45677edb61d
spccoach@gmail.com <spccoach@gmail.com>
2024-12-27 16:19:24 +0000

    Edited pair_functions and pair_reports

--- server_code/pair_functions.py
+++ server_code/pair_functions.py
@@ -6,28 +6,31 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import pandas as pd
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 
-#------------------------------------------------------------
+#------------------------------------------------------------------
 #
 #        Pair Functions 
 #
-#-------------------------------------------------------------
+#------------------------------------------------------------------
+
+#------------------------------------------------------------------
+#           Pair Filter - limit data to only points with disp_pair 
+#------------------------------------------------------------------
 def pair_filter(ppr_df, disp_pair):
   # filter a ppr dataframe down to all plays with the pair palyer 1 and 2
   disp_pair = disp_pair.strip()
-  #print(f" Pair: {disp_pair}")
-  #print("ppr df team a column, then team b")
-  #print(ppr_df['teama'])
-  #print(ppr_df['teamb'])
-  
   tmp_df = ppr_df[ (ppr_df['teama'].str.strip() == disp_pair) |
                    (ppr_df['teamb'].str.strip() == disp_pair) 
                   ]
   return tmp_df
 
+#------------------------------------------------------------------
+#           Pair Players - lireturn the two players in the pair 
+#------------------------------------------------------------------
 def pair_players(disp_pair):
   # search the master pair table to find the two players
   disp_pair = disp_pair.strip()
@@ -36,6 +39,56 @@ def pair_players(disp_pair):
     disp_player2 = pair_row['player2']
   return disp_player1, disp_player2
 
+#------------------------------------------------------------------
+#           Pair Point Totals - return a df with the point totals for the pair 
+#------------------------------------------------------------------  
+def pair_pt_total(ppr_df, disp_pair):
+  # count the total number of points in each category
 
+  # return datafarme with the details
+  pts_dict = {'p_tsa':[0], # this pair, terminal serve ace
+              'p_tse':[0],# this pair, terminal serve error
+              'p_fbk':[0], # this pair, first ball kill
+              'p_fbe':[0], # this pair, first ball error
+              'p_tk':[0], # this pair, transition kills
+              'p_te':[0], # this pair, transition errors
+              'p_serves':[0], # this pair, total number of serves
+              'o_tsa':[0], # opponent - terminal serve ace
+              'o_tse':[0], # opponent - terminal serve error
+              'o_fbk':[0], # opponent - first ball kill
+              'o_fbe':[0], # opponent - first ball error
+              'o_tk':[0], # opponent - transition kill
+              'o_te':[0], # opponent - transition error
+              'o_serves':[0], # opponent - total number of serves
+              'pts_total':[0] # total number of points played
+             }
+
+  pts_df = pd.DataFrame.from_dict( pts_dict )
+  disp_pair = disp_pair.strip() # just to make sure!
+  # filter whoiel dataframe to this player:
+  ppr_df = pair_filter(ppr_df,disp_pair)
+
+  # let's start counting!
+  tmp_df = ppr_df[ppr_df['point_outcome_team'] == disp_pair]
+  pts_df.at[0,'p_tsa'] = tmp_df[ tmp_df['point_outcome'] == 'TSA' ].shape[0]
+  pts_df.at[0,'p_tse'] = tmp_df[ tmp_df['point_outcome'] == 'TSE' ].shape[0]
+  pts_df.at[0,'p_fbk'] = tmp_df[ tmp_df['point_outcome'] == 'FBK' ].shape[0]
+  pts_df.at[0,'p_fbe'] = tmp_df[ tmp_df['point_outcome'] == 'FBE' ].shape[0]
+  pts_df.at[0,'p_tk']  = tmp_df[ tmp_df['point_outcome'] == 'TK'  ].shape[0]
+  pts_df.at[0,'p_te']  = tmp_df[ tmp_df['point_outcome'] == 'TE'  ].shape[0]
+  pts_df.at[0,'p_serves'] = tmp_df[ disp_pair.contains(tmp_df['serve_player']) ]
+  
+  tmp_df = ppr_df[ ~ppr_df['point_outcome_team'] == disp_pair]
+  pts_df.at[0,'o_tsa'] = tmp_df[ tmp_df['point_outcome'] == 'TSA' ].shape[0]
+  pts_df.at[0,'o_tse'] = tmp_df[ tmp_df['point_outcome'] == 'TSE' ].shape[0]
+  pts_df.at[0,'o_fbk'] = tmp_df[ tmp_df['point_outcome'] == 'FBK' ].shape[0]
+  pts_df.at[0,'o_fbe'] = tmp_df[ tmp_df['point_outcome'] == 'FBE' ].shape[0]
+  pts_df.at[0,'o_tk'] = tmp_df[ tmp_df['point_outcome'] == 'TK' ].shape[0]
+  pts_df.at[0,'o_te'] = tmp_df[ tmp_df['point_outcome'] == 'TE' ].shape[0]
+  pts_df.at[0,'o_serves'] = tmp_df[ ~disp_pair.str.contains(tmp_df['serve_player']) ]
+  
+  pts_df.at[0,'pts_total'] = ppr_df.shape[0]
+
+  return pts_df
   
   
--- server_code/pair_reports.py
+++ server_code/pair_reports.py
@@ -117,4 +117,196 @@ def pair_fbhe_net(disp_league, disp_gender, disp_year,
   p2_markdown = pd.DataFrame.to_markdown(p2_fbhe_table)
   
   return pair_markdown, p1_markdown, p2_markdown
+
+#-------------------------------------------------------
+#
+#         Pair Report Stub
+#
+#--------------------------------------------------------
+@anvil.server.callable
+def pair_summary_rpt(disp_league, disp_gender, disp_year, 
+                  disp_team, disp_pair,
+                  comp_l1_checked, disp_comp_l1,
+                  comp_l2_checked, disp_comp_l2,
+                  comp_l3_checked, disp_comp_l3,
+                  date_checked, disp_start_date, disp_end_date,
+                  scout, explain_text
+                ):
+  # note, added disp_pair in the arguments
+  disp_player1, disp_player2 = pair_players(disp_pair) # looks iinto master_pairs to get player 1 and 2 for the given pair
+  ppr_df = get_ppr_data( disp_league, disp_gender, disp_year, disp_team, True ) # gets the ppr data, this should be all the data available to report on
+  ppr_df = ppr_df_limit( ppr_df, 
+                         comp_l1_checked, disp_comp_l1, 
+                         comp_l2_checked, disp_comp_l2, 
+                         comp_l3_checked, disp_comp_l3, 
+                         date_checked, disp_start_date, disp_end_date
+                         ) # limit all data available to the parameters given for comp level 1,2,3 and dates.
+  ppr_df = pair_filter(ppr_df, disp_pair) # lastly, filter the data to all play that include the pair of interest
+
+  scor_dict = {' ':['Pt Diff','Term Srv','Live Rallies', '',
+                    'SideOut', 'Opp Sideout','',
+                    'FB Stop', 'Opp FB Stop','',
+                    'FB Win','',
+                    'Trans Win','Opp Trans Win','',
+                    'Blocking','Digging','Create','',
+                   'Serving','First Ball Attacking','Transition Attacking'],
+             '#':[0,0,0,' ', 0,0,' ',0,0,' ',0,' ',0,0,' ',0,0,0,' ',0,0,0],
+             '%':[0,0,0,' ', 0,0,' ',0,0,' ',0,' ',0,0,' ',0,0,0,' ',0,0,0]
+            }
+  scor_table = pd.DataFrame.from_dict( scor_dict )
+
+  ############### Third Populate the dataframe, assuming we have data returned
+  # Call number of points
+  pts_df = pair_pt_total(ppr_df,disp_pair)
+  
+  if pts_df[0,'pts_total'] != 0:
+    # now store the values into t scor_return array
+    # point differential
+    scor_table.at[0,'#'] = ( ( pts_df.at[0,'p_tsa'] + pts_df.at[0,'p_fbk'] + pts_df.at[0,'p_tk'] + pts_df.at[0,'o_tse'] + pts_df.at[0,'o_fbe'] + pts_df.at[0,'o_te'] ) -
+                          ( pts_df.at[0,'p_tse'] + pts_df.at[0,'p_fbe'] + pts_df.at[0,'p_te'] + pts_df.at[0,'o_tsa'] + pts_df.at[0,'o_fbk'] + pts_df.at[0,'o_tk'] ) )
+    scor_table.at[0,'%'] = scor_table.at[0,'#']/pts_df.at[0,'pts_total']
+
+    # Terminal Serves
+    scor_table.at[1,'#'] = ( pts_df.at[0,'p_tsa'] + pts_df.at[0,'o_tse']) - ( pts_df.at[0,'o_tsa'] + pts_df.at[0,'p_tse'])
+    scor_table.at[1,'%'] = scor_table.at[1,'#']/pts_df.at[0,'pts_total']
+
+    # live Rallies -- guessing all transition points??
+    scor_table.at[2,'#'] = pts_df.at[0,'p_tk'] + pts_df.at[0,'p_te'] + pts_df.at[0,'o_tk'] + pts_df.at[0,'o_te']
+    scor_table.at[2,"%"] = scor_table.at[2,'#'] / pts_df.at[0,'pts_total']
+
+    # blank row
+    # Side out = FBK + TK
+    #----------------------------
+    #... i think we have an issue here as we want TK only when we were served?  Or both TE and TK when we were served?
+    #----------------------
+    scor_table.at[4,'#'] = pts_df.at[0,'p_fbk'] + pts_df.at[0,'p_tk']
+    scor_table.at[4,"%"] = scor_table.at[4,'#'] / pts_df.at[0,'pts_total']
+    scor_table.at[5,'#'] = pts_df.at[0,'o_fbk'] + pts_df.at[0,'o_tk']
+    scor_table.at[5,"%"] = scor_table.at[5,'#'] / pts_df.at[0,'pts_total']
+
+    # first ball sideout - FBK/points
+    scor_table.at[7,"#"] = pts_df.at[0,'p_fbk']
+    scor_table.at[7,"%"] = pts_df.at[0,'p_fbk']/pts_df.at[0,'o_serves']
+    scor_table.at[8,"#"] = pts_df.at[0,'o_fbk']
+    scor_table.at[8,"%"] = pts_df.at[0,'o_fbk']/pts_df.at[0,'p_serves']
+
+    # blank row
+    # first ball stop - fbe / serves (attempts)
+    scor_table.at[10,"#"] = pts_df.at[0,'o_fbe']
+    scor_table.at[10,"%"] = pts_df.at[0,'o_fbe']/(pts_df.at[0,'p_serves'] - pts_df.at[0,'p_tse'])
+    scor_table.at[11,"#"] = pts_df.at[0,'p_fbe']
+    scor_table.at[11,"%"] = pts_df.at[0,'p_fbe']/(pts_df.at[0,'o_serves'] - pts_df.at[0,'o_tse'])
+
+    # blank row
+    # first ball win %
+    scor_table.at[13,'%'] = ( pts_df.at[0,'p_fbk'] + pts_df.at[0,'o_fbe']) / (pts_df.at[0,'pts_total']-pts_df.at[0,'p_tse']-ptd_df.at[0,'o_tse'])
+
+    # blank row
+    #Transition Win - Number of transitiono points we won! and they won
+ 
+    # now create the markdown text to return
+    scor_markdown = pd.DataFrame.to_markdown(scor_table)
+  else:
+    scor_markdown = "No Data Found"
+    # So I think we are done:-)
+  return scor_markdown,  ' ', ' '
+  
+
+#-------------------------------------------------------
+#
+#         Pair Report Stub
+#
+#--------------------------------------------------------
+@anvil.server.callable
+def pair_rpt_stub(disp_league, disp_gender, disp_year, 
+                  disp_team, disp_pair,
+                  comp_l1_checked, disp_comp_l1,
+                  comp_l2_checked, disp_comp_l2,
+                  comp_l3_checked, disp_comp_l3,
+                  date_checked, disp_start_date, disp_end_date,
+                  scout, explain_text
+                ):
+  # note, added disp_pair in the arguments
+
+  disp_player1, disp_player2 = pair_players(disp_pair) # looks iinto master_pairs to get player 1 and 2 for the given pair
+  ppr_df = get_ppr_data( disp_league, disp_gender, disp_year, disp_team, True ) # gets the ppr data, this should be all the data available to report on
+  ppr_df = ppr_df_limit( ppr_df, 
+                         comp_l1_checked, disp_comp_l1, 
+                         comp_l2_checked, disp_comp_l2, 
+                         comp_l3_checked, disp_comp_l3, 
+                         date_checked, disp_start_date, disp_end_date
+                         ) # limit all data available to the parameters given for comp level 1,2,3 and dates.
+  ppr_df = pair_filter(ppr_df, disp_pair) # lastly, filter the data to all play that include the pair of interest
+
+  # create the output dataframe
+  df_dict = {' ':['FBHE','Kills','Errors','Attempts','URL'],
+             'All':[0,0,0,0,' '],
+             'Zone 1':[0,0,0,0,' '],
+             "Zone 2":[0,0,0,0,' '],
+             'Zone 3':[0,0,0,0,' '],
+             'Zone 4':[0,0,0,0,' '],
+             'Zone 5':[0,0,0,0,' ']
+            }
+  pair_table = pd.DataFrame.from_dict( df_dict )
+  p1_table = pd.DataFrame.from_dict( df_dict )
+  p2_table = pd.DataFrame.from_dict( df_dict )
+  
+  # calcualte for player 1 - All attempts
+  fbhe_vector = fbhe(ppr_df,disp_player1,'att',True)
+  p1_table.at[0,'All'] = fbhe_vector[0]  # fbhe
+  p1_table.at[1,'All'] = fbhe_vector[1]  # attacks
+  p1_table.at[2,'All'] = fbhe_vector[2]  # errors
+  p1_table.at[3,'All'] = fbhe_vector[3]  # attempts
+  # ignorinig the confienence interval
+  p1_table.at[4,'All'] = fbhe_vector[5]  # URL
+
+  # calcualte for player 2 - all attempts
+  fbhe_vector = fbhe(ppr_df,disp_player2,'att',True)
+  p2_table.at[0,'All'] = fbhe_vector[0]  # fbhe
+  p2_table.at[1,'All'] = fbhe_vector[1]  # attacks
+  p2_table.at[2,'All'] = fbhe_vector[2]  # errors
+  p2_table.at[3,'All'] = fbhe_vector[3]  # attempts
+  # ignore confidence internval
+  p2_table.at[4,'All'] = fbhe_vector[5]  # URL
+
+  # calculate the df for the pair
+  pair_table.at[1,'All'] = p1_table.at[1,'All'] + p2_table.at[2,'All']
+  pair_table.at[2,'All'] = p1_table.at[2,'All'] + p2_table.at[2,'All']
+  pair_table.at[3,'All'] = p1_table.at[3,'All'] + p2_table.at[3,'All']
+  pair_table.at[0,'All'] = (pair_table.at[1,'All'] - pair_table.at[2,'All']) / (pair_table.at[3,'All'])
+  pair_table.at[0,'All'] = float("{:.3f}".format(pair_table.at[0,'All']))
+
+  # calculate for zones 1 - 5
+  column = ['Zone 1','Zone 2','Zone 3','Zone 4','Zone 5']
+  for i in [1,2,3,4,5]:
+    # calculate for player 1
+    fbhe_vector = fbhe( ppr_df[ppr_df['att_src_zone_net']==i], disp_player1, 'att', True )
+    p1_table.at[0,column[i-1]] = fbhe_vector[0]  # fbhe
+    p1_table.at[1,column[i-1]] = fbhe_vector[1]  # attacks
+    p1_table.at[2,column[i-1]] = fbhe_vector[2]  # errors
+    p1_table.at[3,column[i-1]] = fbhe_vector[3]  # attempts
+    p1_table.at[4,column[i-1]] = fbhe_vector[5]  # URL
+
+    # calculate for player 2
+    fbhe_vector = fbhe( ppr_df[ppr_df['att_src_zone_net']==i], disp_player2, 'att', True )
+    p2_table.at[0,column[i-1]] = fbhe_vector[0]  # fbhe
+    p2_table.at[1,column[i-1]] = fbhe_vector[1]  # attacks
+    p2_table.at[2,column[i-1]] = fbhe_vector[2]  # errors
+    p2_table.at[3,column[i-1]] = fbhe_vector[3]  # attempts
+    p2_table.at[4,column[i-1]] = fbhe_vector[5]  # URL
+
+    # calculate for pair
+    pair_table.at[1,column[i-1]] = p1_table.at[1,column[i-1]] + p2_table.at[1,column[i-1]]
+    pair_table.at[2,column[i-1]] = p1_table.at[2,column[i-1]] + p2_table.at[2,column[i-1]]
+    pair_table.at[3,column[i-1]] = p1_table.at[3,column[i-1]] + p2_table.at[3,column[i-1]]
+    pair_table.at[0,column[i-1]] = (pair_table.at[1,column[i-1]] - pair_table.at[2,column[i-1]]) / (pair_table.at[3,column[i-1]])
+    pair_table.at[0,column[i-1]] = float("{:.3f}".format(pair_table.at[0,column[i-1]]))
+
+    # So I think we are done:-)
+  # now, turn them all into markup
+  pair_markdown = pd.DataFrame.to_markdown(pair_table)
+  p1_markdown = pd.DataFrame.to_markdown(p1_table)
+  p2_markdown = pd.DataFrame.to_markdown(p2_table)
+  
+  return pair_markdown, p1_markdown, p2_markdown
   
