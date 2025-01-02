960876239e2ad74332146ef861cb9cfd6f311212
spccoach@gmail.com <spccoach@gmail.com>
2025-01-02 16:50:05 +0000

    Edited s_w_report and custom package dependencies

--- server_code/s_w_report.py
+++ server_code/s_w_report.py
@@ -7,7 +7,8 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 import io
-import spicy
+import scipy.stats as stats
+import pandas as pd
 
 
 # This is a server module. It runs on the Anvil server,
@@ -22,7 +23,7 @@ import spicy
 #-----------------------------------------------------------------------------#
 
 @anvil.server.callable
-def calc_s_w_player( c_league, c_gender, c_year, c_player ):
+def calc_s_w_player( c_league, c_gender, c_year ):
   #
   # calculate the strengths and weaknesses dataframe based on:
   # - player_data 
@@ -67,32 +68,33 @@ def calc_s_w_player( c_league, c_gender, c_year, c_player ):
               'Criteria Value':[0]
             }
 
-  sw_df = pd_DataFrame.from_dict(s_w_dict)
-  sw_df_new = pd_DataFrame.from_dict(s_w_dict)
+  sw_df = pd.DataFrame.from_dict(sw_dict)
+  sw_df_new = pd.DataFrame.from_dict(sw_dict)
 
   print(f"sw df: {sw_df}")
   print(f"sw df new : {sw_df_new}")
 
   # loop thru the player file
-  for p in (1,pdata_df.shape[0]):
+  print(f"Length of pdata df: {pdata_df.shape[0]}")
+  for p in (0,pdata_df.shape[0]-1):
 
-    print(f"in loop over player data, p=:{p}, player: {pdata_df.at[p,'player']}")
+    print(f"in loop over player data, p=:{p}")
+    print(f"player: {pdata_df.at[p,'player']}")
     
     # loop thru the criteria file
-    for c_index,c_row in app_table.strength_weakness_criteria.search( type = 'player'):
+    for c_row in app_tables.strength_weakness_criteria.search( q.all_of(active=True,type='player')):
       # start making comparisons
       variable = c_row['var']
       var_mean = variable + '_mean'
-      var_sd = variable + '_sd'
+      var_sd = variable + '_stdev'
 
-      print(f"In teh loop over Criteria, Index={index}, variable = {variable}, var mean = {var_mean}, var sd = {var_sd}")
+      print(f"In teh loop over Criteria, variable = {variable}, var mean = {var_mean}, var sd = {var_sd}")
       
       crit_value = pstat_df.at[0,var_mean] + c_row['criteria']*pstat_df.at[0,var_sd]
 
-      print(f"critical value = {crit_value}")
-      
-      if ((c_row['criteria'] > 0 & pdata_df.at[p,variable] >= crit_value) | 
-          (c_row['criteria'] < 0 & pdata_df.at[p,variable] <= crit_value)):
+      print(f"critical value = {crit_value}, mean = {pstat_df.at[0,var_mean]}, StDev = {pstat_df.at[0,var_sd]}, Criteria = {c_row['criteria']}")
+
+      if ((c_row['criteria'] > 0) & (pdata_df.at[p,variable] >= crit_value)): 
         # then add a row to the sw_df dataframe
         print("adding a row to new sw df")
         sw_df_new.at[0,'Player'] = pdata_df.at[p,'player']
@@ -102,24 +104,24 @@ def calc_s_w_player( c_league, c_gender, c_year, c_player ):
         sw_df_new.at[0,'Var Name'] = c_row['var']
         sw_df_new.at[0,'Var Description'] = c_row['var_desc']
         sw_df_new.at[0,'Var Value'] = pdata_df.at[p,variable]
-        sw_df_new.at[0,'Var Percentile'] = norm.ppf( (pdata_df.at[p,variable] - pstat_df.at[0,var_mean] )/ pstat_df.at[0,var_sd])
+        sw_df_new.at[0,'Var Percentile'] = stats.norm.ppf( (pdata_df.at[p,variable] - pstat_df.at[0,var_mean] )/ pstat_df.at[0,var_sd])
         sw_df_new.at[0,'Criteria'] = c_row['criteria']
         sw_df_new.at[0,'Criteria Value'] = crit_value
         print(sw_df_new)
 
         # Now append this to the df
-        sw_df = rbind(sw_df,sw_df_new)
+        sw_df = sw_df.append(sw_df_new)
         print(f"updated sw df:{sw_df}")
 
     # unpack the team, number, and short name from our player defiition
-    p_player = pdata_df[p,'player']
+    p_player = pdata_df.at[p,'player']
     str_loc = p_player.index(' ')
-    p_team = p_player[:str_loc-1].strip()
+    p_team = p_player[:str_loc].strip()
     p_player = p_player[str_loc+1:]
     str_loc = p_player.index(' ')
-    p_num = p_player[:str_loc-1].strip()
+    p_num = p_player[:str_loc].strip()
     p_sname = p_player[str_loc+1:].strip()
-    print(f"Updating the sw_df into the master player for: {p_team}, {p_num}, {p_sname}")
+    print(f"Updating the sw_df into the master player for: {c_league}, {c_gender}, {c_year}, {p_team}, {p_num}, {p_sname}")
     
     # save the dataframe into s_w in master_player
     for mplayer_row in app_tables.master_player.search(
@@ -134,6 +136,7 @@ def calc_s_w_player( c_league, c_gender, c_year, c_player ):
     ):
 
       # convert DF to a media object
+      print(f"Saving SW DF for this player: {p_team}, {p_num},{p_sname}, SW DF: {sw_df}")
       sw_csv_file = pd.DataFrame.to_csv(sw_df)
       sw_media = anvil.BlobMedia(content_type="text/plain", content=sw_csv_file.encode(), name="sw.csv")
       save_result = mplayer_row.update( s_w = sw_media )
