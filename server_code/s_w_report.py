52d3546d8b057824286e9a98704e69c837fee390
spccoach@gmail.com <spccoach@gmail.com>
2025-01-02 13:26:21 +0000

    Edited s_w_report and settings

--- server_code/s_w_report.py
+++ server_code/s_w_report.py
@@ -6,6 +6,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import io
+from spicy.stats import norm
+
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -30,12 +33,29 @@ def calc_s_w_player( c_league, c_gender, c_year, c_player ):
   # then store the dataframe in the master_player file
 
   # open player data, create a data frame from it
-  
-  
-  # open the player stats file for the criteria numbers
+  ppr_csv_row = app_tables.ppr_csv_tables.get( 
+    q.all_of(
+      league = c_league,
+      gender = c_gender,
+      year = c_year,
+      team = 'League'
+      ))
 
+  if ppr_csv_row:
+    pdata_df =  pd.read_csv(io.BytesIO( ppr_csv_row['player_data'].get_bytes()))
+    pstat_df =  pd.read_csv(io.BytesIO( ppr_csv_row['player_data_stats'].get_bytes()))
+    player_data_found= True
+  else:
+    pdata_df = ['']
+    pstat_df = ['']
+    #print('No Team Rows Found')
+    player_data_found = False
+
+  if !player_data_found:
+    return 'No Player Data Found'
+    
   # create the dataframe for the s_w to be stored
-  s_w_dict = {'Player':[''],
+  sw_dict = {'Player':[''],
              'Category':[''], # Category - Strength, Weakness, Of Note
              'Section':[''], # Offense, Defense, Other ...
              'Descripton':[''], # Description of the event
@@ -47,14 +67,58 @@ def calc_s_w_player( c_league, c_gender, c_year, c_player ):
               'Criteria Value':[0]
             }
 
-  s_w_df = pd_DataFrame.from_dict(s_w_dict)
+  sw_df = pd_DataFrame.from_dict(s_w_dict)
+  sw_df_new = pd_DataFrame.from_dict(s_w_dict)
   
   # loop thru the player file
-
+  for p in (1,pdata_df.shape[0]):
+    
     # loop thru the criteria file
+    for c_index,c_row in app_table.strength_weakness_criteria.search( type = 'player'):
+      # start making comparisons
+      variable = c_row['var']
+      var_mean = variable + '_mean'
+      var_sd = variable + '_sd'
+      crit_value = pstat_df.at[0,var_mean] + c_row['criteria']*pstat_df.at[0,var_sd]
+      if ((c_row['criteria'] > 0 & pdata_df.at[p,variable] >= crit_value) | 
+          (c_row['criteria'] < 0 & pdata_df.at[p,variable] <= crit_value)):
+        # then add a row to the sw_df dataframe
+        sw_df_new.at[0,'Player'] = pdata_df.at[p,'player']
+        sw_df_new.at[0,'Category'] = c_row['category']
+        sw_df_new.at[0,'Section'] = c_row['section']
+        sw_df_new.at[0,'Description'] = c_row['description']
+        sw_df_new.at[0,'Var Name'] = c_row['var']
+        sw_df_new.at[0,'Var Description'] = c_row['var_desc']
+        sw_df_new.at[0,'Var Value'] = pdata_df.at[p,variable]
+        sw_df_new.at[0,'Var Percentile'] = norm.ppf( (pdata_df.at[p,variable] - pstat_df.at[0,var_mean] )/ pstat_df.at[0,var_sd])
+        sw_df_new.at[0,'Criteria'] = c_row['criteria']
+        sw_df_new.at[0,'Criteria Value'] = crit_value
 
+        # Now append this to the df
+        sw_df = rbind(sw_df,sw_df_new)
+
+    # unpack the team, number, and short name from our player defiition
+    p_team = 
+    p_num = 
+    p_sname = 
+    
     # save the dataframe into s_w in master_player
+    for mplayer_row in app_tables.master_player.search(
+      q.all_of(
+        league = c_league,
+        gender = c_gender,
+        year = c_year,
+        team = p_team,
+        number = p_num,
+        shortname = p_sname
+      )
+    )
 
+    # convert DF to a media object
+    sw_csv_file = pd.DataFrame.to_csv(sw_df)
+    sw_media = anvil.BlobMedia(content_type="text/plain", content=sw_csv_file.encode(), name="sw.csv")
+    save_result = app_tables.master_player.update( s_w = sw_media )
+  
     # next player
 
   # i think we are all done!
