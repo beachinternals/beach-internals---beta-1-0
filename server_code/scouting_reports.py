9e3adb18318287a994cffe999b8829c6c1b308b5
spccoach@gmail.com <spccoach@gmail.com>
2025-01-14 14:57:33 +0000

    Edited Homepage.ScoutingRpt and scouting_reports

--- server_code/scouting_reports.py
+++ server_code/scouting_reports.py
@@ -6,6 +6,8 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+from server_functions import *
+from pair_functions import *
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -31,8 +33,8 @@ def scout_srv_strategy(disp_league,
                       comp_l3_checked,
                       disp_comp_l3,
                       date_checked,
-                      start_date,
-                      end_date,
+                      disp_start_date,
+                      disp_end_date,
                       scout,
                       explain_text,
                       srv_fr, srv_to_1,srv_to_2,srv_to_3,srv_to_4,srv_to_5
@@ -42,45 +44,45 @@ def scout_srv_strategy(disp_league,
   player_label = disp_player
 
   # lets find the serve strategy Text:
-  if (srv_to_1[0] == True) or (srv_to_1[1] == True) or (srv_to_1[2] == True) or (srv_to_5[0] == True) or (srv_to_5[1] == True) or (srv_to_5[2] == True):
+  if (srv_to_1[0]) or (srv_to_1[1]) or (srv_to_1[2]) or (srv_to_5[0]) or (srv_to_5[1]) or (srv_to_5[2]):
     srv_line = True 
   else:
     srv_line = False
-  if (srv_to_2[0] == True) or (srv_to_2[1] == True) or (srv_to_2[2] == True) or (srv_to_4[0] == True) or (srv_to_4[1] == True) or (srv_to_4[2] == True):
+  if (srv_to_2[0]) or (srv_to_2[1]) or (srv_to_2[2]) or (srv_to_4[0]) or (srv_to_4[1]) or (srv_to_4[2]):
     srv_body = True 
   else:
     srv_body = False
-  if (srv_to_3[0] == True) or (srv_to_3[1] == True) or (srv_to_3[2] == True):
+  if (srv_to_3[0]) or (srv_to_3[1]) or (srv_to_3[2]):
     srv_seam = True 
   else:
     srv_seam = False
 
-  srv_deep =  True if (srv_to_1[0] == True) or (srv_to_2[0] == True) or (srv_to_3[0] == True)or (srv_to_4[0] == True)or (srv_to_5[0] == True) else False
-  srv_mid =   True if (srv_to_1[1] == True) or (srv_to_2[1] == True) or (srv_to_3[1] == True)or (srv_to_4[1] == True)or (srv_to_5[1] == True) else False
-  srv_short = True if (srv_to_1[2] == True) or (srv_to_2[2] == True) or (srv_to_3[2] == True)or (srv_to_4[2] == True)or (srv_to_5[2] == True) else False
+  srv_deep =  True if (srv_to_1[0]) or (srv_to_2[0]) or (srv_to_3[0]) or (srv_to_4[0]) or (srv_to_5[0]) else False
+  srv_mid =   True if (srv_to_1[1]) or (srv_to_2[1]) or (srv_to_3[1]) or (srv_to_4[1]) or (srv_to_5[1]) else False
+  srv_short = True if (srv_to_1[2]) or (srv_to_2[2]) or (srv_to_3[2]) or (srv_to_4[2]) or (srv_to_5[2]) else False
 
   srv_to_text = ''
   if srv_line:
-    srv_to_text = 'Line'
+    srv_to_text = 'Line:'
   elif srv_body:
-    srv_to_text = 'Body'
+    srv_to_text = 'Body:'
   elif srv_seam:
-    srv_to_tesxt = 'Seam'
+    srv_to_text = 'Seam:'
 
   if srv_short:
-    srv_to_text = srv_to_text + ', Short '
-  elif srv_mid:
-    srv_to_text = srv_to_text + ', Mid '
-  elif srv_deep:
-    srv_to_text = srv_to_text + ", Deep "
+    srv_to_text = srv_to_text + ', Short'
+  if srv_mid:
+    srv_to_text = srv_to_text + ', Mid'
+  if srv_deep:
+    srv_to_text = srv_to_text + ", Deep"
 
   srv_fr_text = ''
   if srv_fr[0]:
-    srv_fr_text = 'Left Line'
+    srv_fr_text = 'Line (Left)'
   elif srv_fr[1]:
-    srv_fr_text = "Middle"
+    srv_fr_text = 'Middle'
   elif srv_fr[2]:
-    svr_fr_text = 'Right Line'
+    srv_fr_text = 'Line (Right)'
 
   if srv_fr[0]:
     if srv_fr[1]:
@@ -89,7 +91,7 @@ def scout_srv_strategy(disp_league,
       else:
         svr_fr_list = [1,3]
     elif srv_fr[2]:
-      svr_fr_list = [1,5] 
+      srv_fr_list = [1,5] 
   elif srv_fr[1]:
     if srv_fr[2]:
       srv_fr_list = [3,4]
@@ -99,22 +101,28 @@ def scout_srv_strategy(disp_league,
     srv_fr_list = [5]
 
   srv_strategy_title = 'Serving '+disp_player+' from '+srv_fr_text+' to '+srv_to_text
-
-  # make a list of tuples for hte serve to zones
+  print(f"Serving Strategy: {srv_strategy_title}")
+  
+  # make a list of tuples for the serve to zones
   srv_2 = []
+  print(f"Serve To 1:{srv_to_1}")
+  print(f"Serve To 2:{srv_to_2}")
+  print(f"Serve To 3:{srv_to_3}")
+  print(f"Serve To 4:{srv_to_4}")
+  print(f"Serve To 5:{srv_to_5}")
   for j in (0,1,2):
-    if serve_to_1[j]:
+    if srv_to_1[j]:
       srv_2.append([1,j])
-    if serve_to_2[j]:
+    if srv_to_2[j]:
       srv_2.append([2,j])
-    if serve_to_3[j]:
+    if srv_to_3[j]:
       srv_2.append([3,j])
-    if serve_to_4[j]:
+    if srv_to_4[j]:
       srv_2.append([4,j])
-    if serve_to_5[j]:
+    if srv_to_5[j]:
       srv_2.append([5,j])
   # let's see what we have
-  print(f"svr list of tubles {srv_2}")
+  print(f"svr list of tuples {srv_2}")
   
   # get the ppr data
   ppr_df =  get_ppr_data( disp_league, disp_gender, disp_year, disp_team, True ) # gets the ppr data, this should be all the data available to report on
@@ -137,33 +145,34 @@ def scout_srv_strategy(disp_league,
       ppr_df_n.append(ppr_df_s2)
 
   # filter the data by serve sones
-  ppr_df = ppr_df[ (ppr_df['serve_src_zone_net'] == 1 & srv_fr[0] ) | 
-                   (ppr_df['serve_src_zone_net'] == 3 & srv_fr[1] ) |
-                   (ppr_df['serve_src_zone_net'] == 5 & srv_fr[2] )
+  ppr_df = ppr_df[ ((ppr_df['serve_src_zone_net'] == 1) & (srv_fr[0] )) | 
+                   ((ppr_df['serve_src_zone_net'] == 3) & (srv_fr[1] )) |
+                   ((ppr_df['serve_src_zone_net'] == 5) & (srv_fr[2] ))
                     ]
 
   # filter the data by serve destination zones
-  ppr_df = ppr_df[ (ppr_df['serve_dest_zone_net'] == 1 & ppr_df['serve_dest_zone_depth'] == 'E' & srv_to_1[0] ) | 
-                   (ppr_df['serve_dest_zone_net'] == 1 & ppr_df['serve_dest_zone_depth'] == 'D' & srv_to_1[1] ) |
-                   (ppr_df['serve_dest_zone_net'] == 1 & ppr_df['serve_dest_zone_depth'],isin(['A','B','C']) & srv_to_1[3] )
-                   (ppr_df['serve_dest_zone_net'] == 2 & ppr_df['serve_dest_zone_depth'] == 'E' & srv_to_2[0] ) | 
-                   (ppr_df['serve_dest_zone_net'] == 2 & ppr_df['serve_dest_zone_depth'] == 'D' & srv_to_2[1] ) |
-                   (ppr_df['serve_dest_zone_net'] == 2 & ppr_df['serve_dest_zone_depth'],isin(['A','B','C']) & srv_to_2[3] )
-                   (ppr_df['serve_dest_zone_net'] == 3 & ppr_df['serve_dest_zone_depth'] == 'E' & srv_to_3[0] ) | 
-                   (ppr_df['serve_dest_zone_net'] == 3 & ppr_df['serve_dest_zone_depth'] == 'D' & srv_to_3[1] ) |
-                   (ppr_df['serve_dest_zone_net'] == 3 & ppr_df['serve_dest_zone_depth'],isin(['A','B','C']) & srv_to_3[3] )
-                   (ppr_df['serve_dest_zone_net'] == 4 & ppr_df['serve_dest_zone_depth'] == 'E' & srv_to_4[0] ) | 
-                   (ppr_df['serve_dest_zone_net'] == 4 & ppr_df['serve_dest_zone_depth'] == 'D' & srv_to_4[1] ) |
-                   (ppr_df['serve_dest_zone_net'] == 4 & ppr_df['serve_dest_zone_depth'],isin(['A','B','C']) & srv_to_4[3] )
-                   (ppr_df['serve_dest_zone_net'] == 5 & ppr_df['serve_dest_zone_depth'] == 'E' & srv_to_5[0] ) | 
-                   (ppr_df['serve_dest_zone_net'] == 5 & ppr_df['serve_dest_zone_depth'] == 'D' & srv_to_5[1] ) |
-                   (ppr_df['serve_dest_zone_net'] == 5 & ppr_df['serve_dest_zone_depth'],isin(['A','B','C']) & srv_to_5[3] )
+  ppr_df = ppr_df[ ((ppr_df['serve_dest_zone_net'] == 1 & ppr_df['serve_dest_zone_depth'] == 'E' & srv_to_1[0] )) | 
+                   ((ppr_df['serve_dest_zone_net'] == 1 & ppr_df['serve_dest_zone_depth'] == 'D' & srv_to_1[1] )) |
+                   ((ppr_df['serve_dest_zone_net'] == 1 & ppr_df['serve_dest_zone_depth'].isin(['A','B','C']) & srv_to_1[3] )) |
+                   ((ppr_df['serve_dest_zone_net'] == 2 & ppr_df['serve_dest_zone_depth'] == 'E' & srv_to_2[0] )) | 
+                   ((ppr_df['serve_dest_zone_net'] == 2 & ppr_df['serve_dest_zone_depth'] == 'D' & srv_to_2[1] )) |
+                   ((ppr_df['serve_dest_zone_net'] == 2 & ppr_df['serve_dest_zone_depth'].isin(['A','B','C']) & srv_to_2[3] ))
+                   ((ppr_df['serve_dest_zone_net'] == 3 & ppr_df['serve_dest_zone_depth'] == 'E' & srv_to_3[0] )) | 
+                   ((ppr_df['serve_dest_zone_net'] == 3 & ppr_df['serve_dest_zone_depth'] == 'D' & srv_to_3[1] )) |
+                   ((ppr_df['serve_dest_zone_net'] == 3 & ppr_df['serve_dest_zone_depth'].isin(['A','B','C']) & srv_to_3[3] )) |
+                   ((ppr_df['serve_dest_zone_net'] == 4 & ppr_df['serve_dest_zone_depth'] == 'E' & srv_to_4[0] )) | 
+                   ((ppr_df['serve_dest_zone_net'] == 4 & ppr_df['serve_dest_zone_depth'] == 'D' & srv_to_4[1] )) |
+                   ((ppr_df['serve_dest_zone_net'] == 4 & ppr_df['serve_dest_zone_depth'].isin(['A','B','C']) & srv_to_4[3] )) |
+                   ((ppr_df['serve_dest_zone_net'] == 5 & ppr_df['serve_dest_zone_depth'] == 'E' & srv_to_5[0] )) | 
+                   ((ppr_df['serve_dest_zone_net'] == 5 & ppr_df['serve_dest_zone_depth'] == 'D' & srv_to_5[1] )) |
+                   ((ppr_df['serve_dest_zone_net'] == 5 & ppr_df['serve_dest_zone_depth'].isin(['A','B','C']) & srv_to_5[3] )) 
                     ]
 
   print(f"Finally, we have {ppr_df.shape[0]} serves to analyze")
   
   # calculate a quick table FBHE
   fbhe_vector = fbhe(ppr_df, disp_pair, 'att',True)
+  print(f"fbhe Vector: {fbhe_vector}")
   srv_strat_dict = {'From':[0],
                      'To':[0],
                     'Attempts':[0],
@@ -172,11 +181,5 @@ def scout_srv_strategy(disp_league,
                    }
   srv_strat_df = pd.DataFrame.from_dict(srv_strat_dict)
   print(f"Srv Strat DF: {srv_strat_df}")
-  
-  serves_plot
-  pass_loc_plot
-  set_loc_plot
-  rpt_disp_box = 
-
 
   return
