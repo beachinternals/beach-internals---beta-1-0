9898ead6a3f2eeb04ad440582b3bf69dddf23fda
spccoach@gmail.com <spccoach@gmail.com>
2025-01-15 14:57:29 +0000

    Edited pair_reports

--- server_code/pair_reports.py
+++ server_code/pair_reports.py
@@ -128,6 +128,155 @@ def pair_fbhe_net(disp_league, disp_gender, disp_year,
   
   return pair_markdown, p1_markdown, p2_markdown
 
+
+
+@anvil.server.callable
+def pair_fbhe_pass(disp_league, disp_gender, disp_year, 
+                  disp_team, disp_pair,
+                  comp_l1_checked, disp_comp_l1,
+                  comp_l2_checked, disp_comp_l2,
+                  comp_l3_checked, disp_comp_l3,
+                  date_checked, disp_start_date, disp_end_date,
+                  scout, explain_text
+                ):
+
+  # First, get the data, and narrow to the filter, now, limit to only pair players
+  disp_player1, disp_player2 = pair_players(disp_pair)
+  #print(f"Pair: {disp_pair}, P1: {disp_player1}, P2: {disp_player2}")
+  ppr_df = get_ppr_data( disp_league, disp_gender, disp_year, disp_team, True )
+  #print(f"ppr_df size: {ppr_df.shape[0]}")
+  ppr_df = ppr_df_limit( ppr_df, 
+                         comp_l1_checked, disp_comp_l1, 
+                         comp_l2_checked, disp_comp_l2, 
+                         comp_l3_checked, disp_comp_l3, 
+                         date_checked, disp_start_date, disp_end_date
+                         )
+  #print(f"ppr_df size after limits: {ppr_df.shape[0]}")
+  ppr_df = pair_filter(ppr_df, disp_pair)
+  ppr_df_no_option = ppr_df[ ppr_df['tactic'] != 'option']
+  ppr_df_option = ppr_df[ ppr_df['tactic'] == 'option']
+  #print(f"ppr_df size after pair only points: {ppr_df.shape[0]}")
+
+  # create the output dataframe
+  df_dict = {' ':['FBHE','FBSO','Kills','Errors','Attempts','URL','% out of System'],
+             'All':[0,0,0,0,0,' ',0],
+             'Zone 1':[0,0,0,0,0,' ',0],
+             "Zone 2":[0,0,0,0,0,' ',0],
+             'Zone 3':[0,0,0,0,0,' ',0],
+             'Zone 4':[0,0,0,0,0,' ',0],
+             'Zone 5':[0,0,0,0,0,' ',0],
+             'Option':[0,0,0,0,0,' ',0]
+            }
+  pair_fbhe_table = pd.DataFrame.from_dict( df_dict )
+  p1_fbhe_table = pd.DataFrame.from_dict( df_dict )
+  p2_fbhe_table = pd.DataFrame.from_dict( df_dict )
+  
+  # calcualte for player 1
+  fbhe_vector = fbhe(ppr_df_no_option,disp_player1,'pass',True)
+  p1_fbhe_table.at[0,'All'] = fbhe_vector[0]  # fbhe
+  p1_fbhe_table.at[1,'All'] = fbhe_vector[4]  # fbso
+  p1_fbhe_table.at[2,'All'] = fbhe_vector[1]  # attacks
+  p1_fbhe_table.at[3,'All'] = fbhe_vector[2]  # errors
+  p1_fbhe_table.at[4,'All'] = fbhe_vector[3]  # attempts
+  #p1_fbhe_table.at[4,'All'] = fbhe_vector[4]  # confidence interval
+  p1_fbhe_table.at[5,'All'] = fbhe_vector[5]  # URL
+
+  # calcualte for player 2
+  fbhe_vector = fbhe(ppr_df_no_option,disp_player2,'pass',True)
+  p2_fbhe_table.at[0,'All'] = fbhe_vector[0]  # fbhe
+  p2_fbhe_table.at[1,'All'] = fbhe_vector[4]  # fbso
+  p2_fbhe_table.at[2,'All'] = fbhe_vector[1]  # attacks
+  p2_fbhe_table.at[3,'All'] = fbhe_vector[2]  # errors
+  p2_fbhe_table.at[4,'All'] = fbhe_vector[3]  # attempts
+  #p2_fbhe_table.at[4,'All'] = fbhe_vector[4]  # confidence interval
+  p2_fbhe_table.at[5,'All'] = fbhe_vector[5]  # URL
+
+  # calculate the df for the pair
+  pair_fbhe_table.at[2,'All'] = p1_fbhe_table.at[2,'All'] + p2_fbhe_table.at[2,'All']
+  pair_fbhe_table.at[3,'All'] = p1_fbhe_table.at[3,'All'] + p2_fbhe_table.at[3,'All']
+  pair_fbhe_table.at[4,'All'] = p1_fbhe_table.at[4,'All'] + p2_fbhe_table.at[4,'All']
+  if pair_fbhe_table.at[4,'All'] != 0:
+    pair_fbhe_table.at[0,'All'] = (pair_fbhe_table.at[2,'All'] - pair_fbhe_table.at[3,'All']) / (pair_fbhe_table.at[4,'All'])
+    pair_fbhe_table.at[1,'All'] = (pair_fbhe_table.at[2,'All']) / (pair_fbhe_table.at[4,'All'])
+  pair_fbhe_table.at[0,'All'] = float("{:.3f}".format(pair_fbhe_table.at[0,'All']))
+  pair_fbhe_table.at[1,'All'] = float("{:.3f}".format(pair_fbhe_table.at[1,'All']))
+
+  # now, zones 1 thru 5
+  # calculate for zones 1 - 5
+  column = ['Zone 1','Zone 2','Zone 3','Zone 4','Zone 5']
+
+
+  for i in [1,2,3,4,5]:
+    # calculate for player 1
+    fbhe_vector = fbhe( ppr_df_no_option[ppr_df_no_option['att_src_zone_net']==i], disp_player1, 'pass', True )
+    p1_fbhe_table.at[0,column[i-1]] = fbhe_vector[0]  # fbhe
+    p1_fbhe_table.at[1,column[i-1]] = fbhe_vector[4]  # fbhe
+    p1_fbhe_table.at[2,column[i-1]] = fbhe_vector[1]  # attacks
+    p1_fbhe_table.at[3,column[i-1]] = fbhe_vector[2]  # errors
+    p1_fbhe_table.at[4,column[i-1]] = fbhe_vector[3]  # attempts
+    p1_fbhe_table.at[5,column[i-1]] = fbhe_vector[5]  # URL
+
+    # calculate for player 2
+    fbhe_vector = fbhe( ppr_df_no_option[ppr_df_no_option['att_src_zone_net']==i], disp_player2, 'pass', True )
+    p2_fbhe_table.at[0,column[i-1]] = fbhe_vector[0]  # fbhe
+    p2_fbhe_table.at[1,column[i-1]] = fbhe_vector[0]  # fbhe
+    p2_fbhe_table.at[2,column[i-1]] = fbhe_vector[1]  # attacks
+    p2_fbhe_table.at[3,column[i-1]] = fbhe_vector[2]  # errors
+    p2_fbhe_table.at[4,column[i-1]] = fbhe_vector[3]  # attempts
+    p2_fbhe_table.at[5,column[i-1]] = fbhe_vector[5]  # URL
+
+    # calculate for pair
+    pair_fbhe_table.at[2,column[i-1]] = p1_fbhe_table.at[2,column[i-1]] + p2_fbhe_table.at[2,column[i-1]]
+    pair_fbhe_table.at[3,column[i-1]] = p1_fbhe_table.at[3,column[i-1]] + p2_fbhe_table.at[3,column[i-1]]
+    pair_fbhe_table.at[4,column[i-1]] = p1_fbhe_table.at[4,column[i-1]] + p2_fbhe_table.at[4,column[i-1]]
+    if ( pair_fbhe_table.at[4,column[i-1]]) != 0:
+      pair_fbhe_table.at[0,column[i-1]] = (pair_fbhe_table.at[2,column[i-1]] - pair_fbhe_table.at[3,column[i-1]]) / (pair_fbhe_table.at[4,column[i-1]])
+      pair_fbhe_table.at[1,column[i-1]] = (pair_fbhe_table.at[2,column[i-1]] ) / (pair_fbhe_table.at[4,column[i-1]])
+    pair_fbhe_table.at[0,column[i-1]] = float("{:.3f}".format(pair_fbhe_table.at[0,column[i-1]]))
+    pair_fbhe_table.at[1,column[i-1]] = float("{:.3f}".format(pair_fbhe_table.at[1,column[i-1]]))
+
+  # now calcualte for the options
+  # calcualte for player 1
+  fbhe_vector = fbhe(ppr_df_option,disp_player1,'pass',True)
+  p1_fbhe_table.at[0,'Option'] = fbhe_vector[0]  # fbhe
+  p1_fbhe_table.at[1,'Option'] = fbhe_vector[4]  # fbso
+  p1_fbhe_table.at[2,'Option'] = fbhe_vector[1]  # attacks
+  p1_fbhe_table.at[3,'Option'] = fbhe_vector[2]  # errors
+  p1_fbhe_table.at[4,'Option'] = fbhe_vector[3]  # attempts
+  #p1_fbhe_table.at[4,'Option'] = fbhe_vector[4]  # confidence interval
+  p1_fbhe_table.at[5,'Option'] = fbhe_vector[5]  # URL
+
+  # calcualte for player 2
+  fbhe_vector = fbhe(ppr_df_option,disp_player2,'pass',True)
+  p2_fbhe_table.at[0,'Option'] = fbhe_vector[0]  # fbhe
+  p2_fbhe_table.at[1,'Option'] = fbhe_vector[4]  # fbso
+  p2_fbhe_table.at[2,'Option'] = fbhe_vector[1]  # attacks
+  p2_fbhe_table.at[3,'Option'] = fbhe_vector[2]  # errors
+  p2_fbhe_table.at[4,'Option'] = fbhe_vector[3]  # attempts
+  #p2_fbhe_table.at[4,'Option'] = fbhe_vector[4]  # confidence interval
+  p2_fbhe_table.at[5,'Option'] = fbhe_vector[5]  # URL
+
+  # calculate the df for the pair
+  pair_fbhe_table.at[2,'Option'] = p1_fbhe_table.at[2,'Option'] + p2_fbhe_table.at[2,'Option']
+  pair_fbhe_table.at[3,'Option'] = p1_fbhe_table.at[3,'Option'] + p2_fbhe_table.at[3,'Option']
+  pair_fbhe_table.at[4,'Option'] = p1_fbhe_table.at[4,'Option'] + p2_fbhe_table.at[4,'Option']
+  if pair_fbhe_table.at[4,'Option'] != 0:
+    pair_fbhe_table.at[0,'Option'] = (pair_fbhe_table.at[2,'Option'] - pair_fbhe_table.at[3,'Option']) / (pair_fbhe_table.at[4,'Option'])
+    pair_fbhe_table.at[1,'Option'] = (pair_fbhe_table.at[2,'Option']) / (pair_fbhe_table.at[4,'Option'])
+  pair_fbhe_table.at[0,'Option'] = float("{:.3f}".format(pair_fbhe_table.at[0,'Option']))
+  pair_fbhe_table.at[1,'Option'] = float("{:.3f}".format(pair_fbhe_table.at[1,'Option']))
+  
+  # So I think we are done:-)
+  # now, turn them all into markup
+  pair_markdown = pd.DataFrame.to_markdown(pair_fbhe_table, index = False )
+  p1_markdown = pd.DataFrame.to_markdown(p1_fbhe_table, index = False )
+  p2_markdown = pd.DataFrame.to_markdown(p2_fbhe_table, index = False )
+  
+  return pair_markdown, p1_markdown, p2_markdown
+
+
+
+
 #-------------------------------------------------------
 #
 #         Pair Summary Repport
