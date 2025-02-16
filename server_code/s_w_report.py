d35419455318db9d27260e251325a6773bd69f5c
spccoach@gmail.com <spccoach@gmail.com>
2025-02-16 18:57:03 +0000

    Edited s_w_report

--- server_code/s_w_report.py
+++ server_code/s_w_report.py
@@ -110,7 +110,7 @@ def calc_s_w_player( c_league, c_gender, c_year ):
 
       if (((c_row['criteria'] > 0) & (pdata_df.at[p,variable] >= crit_value)) | ((c_row['criteria'] < 0) & (pdata_df.at[p,variable] <= crit_value )) | (c_row['criteria'] == 0)): 
         # then add a row to the sw_df dataframe
-        print("adding a row to new sw df, p:{p}")
+        #print("adding a row to new sw df, p:{p}")
         sw_df_new.at[0,'Player'] = pdata_df.at[p,'player']
         sw_df_new.at[0,'Category'] = c_row['category']
         sw_df_new.at[0,'Section'] = c_row['section']
@@ -282,7 +282,7 @@ def calc_s_w_pair( c_league, c_gender, c_year ):
       # added an 'or' for a 0 criteria, implying that this line should be included. This is used to have the actual value of a variable show up in the s_w table
       if (((c_row['criteria'] > 0) & (pdata_df.at[p,variable] >= crit_value)) | ((c_row['criteria'] < 0) & (pdata_df.at[p,variable] <= crit_value )) | (c_row['criteria'] == 0)): 
         # then add a row to the sw_df dataframe
-        print(f"adding a row to new sw df, p:{p}, {c_row['var']}, {c_row['criteria']}")
+        #print(f"adding a row to new sw df, p:{p}, {c_row['var']}, {c_row['criteria']}")
         sw_df_new.at[0,'Pair'] = pdata_df.at[p,'pair']
         sw_df_new.at[0,'Player'] = pdata_df.at[p,'player']
         sw_df_new.at[0,'Category'] = c_row['category']
